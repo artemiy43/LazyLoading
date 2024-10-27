423bbbdf0ea0fba2bbe47418d770421f8f5ed1a1
Artem <artemiy.bondar49@gmail.com>
2024-10-27 17:47:02 +0300

    fix2

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,7 +2,7 @@ import { Item } from "../store/itemStore";
 export function modify(arr: Item[]) {
   //console.log(arr);
   const arrNew = arr.map((item: Item, index: number) => {
-    return { id: index, ...item };
+    return { ...item, id: index };
   });
   return arrNew;
 }
