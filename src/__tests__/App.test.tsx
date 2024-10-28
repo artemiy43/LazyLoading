f5a46d5d2aa0eab1006d4336eee0a06b55486cf9
Artem <artemiy.bondar49@gmail.com>
2024-10-28 23:52:34 +0300

    5

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+// function renderApp() {
+//   render(<App />);
+// }
+
+test("demo", () => {
+  expect(true).toBe(true);
+});
+
+test("Renders the main page", () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
